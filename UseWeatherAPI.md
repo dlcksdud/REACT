9525a61ddf0754f4e480c6c558cecf539add8d89
dlcksdud <cksdud5087@naver.com>
2025-01-29 16:03:01 +0900

    날씨앱 만들기 : 현위치 날씨가져오기 완료

---
+++ UseWeatherAPI.md
@@ -0,0 +1,14 @@
+# Weather Open API로 날씨앱 만들기
+---
+## 1. Weather API 문서
+[Weather open API 문서 링크](https://openweathermap.org/api)
+- api key를 얻기 위해서 사이트에 회원가입하여 로그인한다.
+
+
+## 2. Logic
+- user는 현재위치의 날씨를 볼 수 있다. (지역, 온도, 날씨, 상태)
+- user는 다른 도시의 버튼들을 볼 수 있다.
+- user는 다른 도시 버튼을 클릭하면 해당 도시의 날씨 정보를 볼 수 있다.
+- user는 데이터가 로딩될 떄 로딩 스피너를 볼 수 있다.
+
+
---
+++ first-react/src/App.js
@@ -0,0 +1,67 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios"; // npm install axios
+
+/**
+ * [날씨앱 만들기]
+ * 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
+ * 2. 날씨정보에는 도시, 섭씨 화씨 , 날씨 상태
+ * 3. 아래에는 5개의 버튼 : 1개는 현재위치, 나머지 4개는 다른 도시
+ * 4. 도시버튼을 클릭할 때마다 도시별 날씨가 나온다.
+ * 5. 현재위치버튼(current location)을 누르면 다시 현재위치 기반의 날씨가 나온다.
+ * 6. 데이터를 들고 오는 동안 로딩스피너가 돈다.
+ */
+function App() {
+  // 1. 앱이 실행되자마자 현재 위치 기반의 날씨가 보인다.
+  const [city, setCity] = useState("");
+  const [cloud, setCloud] = useState("");
+  const [tempF, setTempF] = useState("");
+  const [tempC, setTempC] = useState("");
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, [])
+
+  const getCurrentLocation= () => {
+    console.log("getCureentLocation");
+    // 현재위치 가져오기(위도, 경도): js의 navigator 사용
+    // https://www.w3schools.com/html/html5_geolocation.asp
+    navigator.geolocation.getCurrentPosition((position) => {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        console.log("현위치 : ",latitude, longitude);
+        // 현재위치의 날씨 가져오기
+        getCurrentLocationWeather(latitude, longitude);
+    });
+  }
+
+  // 날씨 api 이용 : UseWeatherAPI.md 참고
+  const getCurrentLocationWeather = (latitude, longitude) => {
+    // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e8c53d0373070c7d2fc6f2a23d108dbb`)
+    .then((response) => {
+        console.log("response : ", response);
+        console.log("도시명 : ", response.data.name);
+        console.log("구름 : ", response.data.weather[0].description);
+        console.log("화씨 : ", response.data.main.temp);
+        setCity(response.data.name);
+        setCloud(response.data.weather[0].description);
+        setTempF(response.data.main.temp / 10);
+        setTempC(((tempF - 32) * 5 / 9).toFixed(1));
+    })
+    .catch((error) => {
+        console.log("error : ", error);
+    });
+  };
+
+  return (
+    <div>
+      <h1>도시명 : {city}</h1>
+      <h2>상태 : {cloud}</h2>
+      <p>화씨 : {tempF}</p>
+      <p>섭씨 : {tempC}</p>
+    </div>
+  );
+}
+
+export default App;
