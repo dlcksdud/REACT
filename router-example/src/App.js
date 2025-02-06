04c379706bb3fe1e3dfda0b6f3f97c1266c2e17c
unknown <cksdud5087@naver.com>
2025-02-06 15:31:35 +0900

    react-router 개발 중

--- router-example/src/App.js
+++ router-example/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Homepage from './page/Homepage';
+import AboutPage from './page/AboutPage';
 
 function App() {
   return (
     <div>
-      
+      <Homepage></Homepage>
+      <AboutPage></AboutPage>
     </div>
   );
 }
