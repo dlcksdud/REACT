12b93b98f7773d2a48fe7aa1acb49e7f2f721221
unknown <cksdud5087@naver.com>
2025-02-07 15:07:25 +0900

    react-router 이용한 쇼핑몰 사이트 만들기 | readme.md 추가

---
+++ hnm-react-router-practice/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import ProductAllPage from './Page/ProductAllPage';
+import LoginPage from './Page/LoginPage';
+import ProductDetailPage from './Page/ProductDetailPage';
+import Navbar from './component/Navbar';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar /> {/**navigation bar 만들기 : route 화면은 바뀌어도 navbar는 유지 */}
+      <Routes>
+        <Route path="/" element={<ProductAllPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/product/:id" element={<ProductDetailPage/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ hnm-react-router-practice/src/component/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/**
+ * [Navbar 구성]
+ * <div/> 3개
+ * 1번 <div/>에는 아이콘, 로그인/아웃 버튼
+ * 2번 <div/>에는 HNM 로고
+ * 3번 <div/>에는 카테고리, 검색창
+ */
+const Navbar = () => {
+  return (
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
